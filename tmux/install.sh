031cb539d910da220abae5bb1697efe7b877e12b
tourist <40332842+Ps7ch3@users.noreply.github.com>
2022-12-07 14:36:44 +0800

    feat(tmux): add tpm and enable plugins

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,3 +3,6 @@ cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+# install tpm for plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
