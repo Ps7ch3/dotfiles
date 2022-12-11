2417b09a302d7abe847885fcd6bf75bb2d18b526
tourist <40332842+Ps7ch3@users.noreply.github.com>
2022-12-12 00:38:03 +0800

    feat(nvim): add autopair hook for cmp

--- nvim/lua/plugin-configs/nvim-autopairs.lua
+++ nvim/lua/plugin-configs/nvim-autopairs.lua
@@ -5,17 +5,28 @@ if not ok then
 end
 
 autopairs.setup({
-    disable_filetype = { "TelescopePrompt" , "vim" },
+    disable_filetype = { "TelescopePrompt" },
     enable_check_bracket_line = true,
     fast_wrap = {
-      map = '<M-e>',
-      chars = { '{', '[', '(', '"', "'" },
-      pattern = [=[[%'%"%)%>%]%)%}%,]]=],
-      end_key = '$',
-      keys = 'qwertyuiopzxcvbnmasdfghjkl',
-      check_comma = true,
-      highlight = 'Search',
-      highlight_grey='Comment'
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+        end_key = '$',
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        check_comma = true,
+        highlight = 'Search',
+        highlight_grey = 'Comment'
     }
 })
 
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+    return
+end
+
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+)
