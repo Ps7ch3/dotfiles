5f43c7610c84073dd65a64f85524069a4c2a93a5
tourist <40332842+Ps7ch3@users.noreply.github.com>
2022-12-12 02:57:31 +0800

    feat(nvim): add editorconfig plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -63,6 +63,7 @@ return packer.startup(function(use)
     use {"nvim-telescope/telescope.nvim", tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} }}
     use {"akinsho/toggleterm.nvim", tag = '*'}
     use {"folke/which-key.nvim"}
+    use {"gpanders/editorconfig.nvim"}
 
     -- code
     use 'windwp/nvim-autopairs'
