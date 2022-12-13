5cc1f6962319ab6ffd58f9416c907b73a1829ae6
tourist <40332842+Ps7ch3@users.noreply.github.com>
2022-12-13 21:56:00 +0800

    refactor(nvim): init vim.opt with for loop

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -10,18 +10,21 @@ local undodir = vim.fn.stdpath('data') .. '/undodir'
 vim.fn.mkdir(undodir, 'p')
 vim.o.undodir = undodir
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-vim.opt.encoding="utf-8"
-vim.o.background = "dark"
-
 -- cmd
 vim.cmd "syntax on"
 vim.cmd "set number"
 vim.cmd "set undofile"
 
--- global option
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.smartindent = true
-vim.o.expandtab = true
+
+local options = {
+    tabstop = 4,
+    shiftwidth = 4,
+    smartindent = true,
+    expandtab = true,
+    encoding = "utf-8",
+    termguicolors = true,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
