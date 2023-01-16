7a284bb262e4f1d9060a10d86a5ebd99d07d4f07
tourist <40332842+Ps7ch3@users.noreply.github.com>
2023-01-16 22:16:27 +0800

    fix(nvim): wrong keymap for telescope git_files

--- nvim/lua/plugin-configs/telescope.lua
+++ nvim/lua/plugin-configs/telescope.lua
@@ -6,7 +6,7 @@ end
 local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
