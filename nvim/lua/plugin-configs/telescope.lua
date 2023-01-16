f0a67ff16ad598723d0652ed590c8d117eb67268
tourist <40332842+Ps7ch3@users.noreply.github.com>
2023-01-16 21:35:29 +0800

    fix(nvim): wrong file ignore pattern for telescope

--- nvim/lua/plugin-configs/telescope.lua
+++ nvim/lua/plugin-configs/telescope.lua
@@ -6,6 +6,7 @@ end
 local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
@@ -14,9 +15,7 @@ telescope.setup {
     defaults = {
         path_display = { "smart" },
         file_ignore_patterns = {
-            ".git/",
-            ".venv/",
-            ".pyvenv/",
+            "^./.git/",
         }
     },
     pickers = {
