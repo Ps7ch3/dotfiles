3b9d2f00428f17ac342cacb56d565eccc40becd9
tourist <40332842+Ps7ch3@users.noreply.github.com>
2023-01-16 21:41:00 +0800

    feat(nvim): add harpoon keymap

--- nvim/lua/plugin-configs/harpoon.lua
+++ nvim/lua/plugin-configs/harpoon.lua
@@ -11,6 +11,11 @@ end
 vim.keymap.set("n", "<leader>af", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
+vim.keymap.set("n", "<leader>q", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>w", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>e", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>r", function() ui.nav_file(4) end)
+
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
     return
